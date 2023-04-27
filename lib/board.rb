b947e5704737adb2c3b021d95b63edcc89f95701
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-04-27 17:52:43 +0200

    changing boardcase assignements

--- lib/board.rb
+++ lib/board.rb
@@ -5,13 +5,13 @@ class Board
     @grid = []
     @grid += [
       BoardCase.new("A1"),
-      BoardCase.new("B1"),
-      BoardCase.new("C1"),
       BoardCase.new("A2"),
-      BoardCase.new("B2"),
-      BoardCase.new("C2"),
       BoardCase.new("A3"),
+      BoardCase.new("B1"),
+      BoardCase.new("B2"),
       BoardCase.new("B3"),
+      BoardCase.new("C1"),
+      BoardCase.new("C2"),
       BoardCase.new("C3")
     ]
 
