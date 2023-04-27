c86e9d6f8209a35ad41b58e24e69144b8e40ac2f
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-04-27 16:49:17 +0200

    application implementation

--- app.rb
+++ app.rb
@@ -6,14 +6,7 @@ require_relative 'lib/board'
 require_relative 'lib/boardcase'
 require_relative 'lib/player'
 require_relative 'lib/show'
+require_relative 'lib/application'
 
 
-puts "What's your name?"
-player1 = gets.chomp
-puts "What's your name?"
-player2 = gets.chomp
-
-
-new_game = Game.new(player1, player2)
-new_game.turn
-new_game.new_round
+Application.new.perform
--- lib/application.rb
+++ lib/application.rb
@@ -1,8 +1,20 @@
 class Application
   def perform
     # TO DO : méthode qui initialise le jeu puis contient des boucles while pour faire tourner le jeu tant que la partie n'est pas terminée.
-    # 
+    puts "What's your name?"
+    player1 = gets.chomp
+    puts "What's your name?"
+    player2 = gets.chomp
 
+    new_game = Game.new(player1, player2)
+    new_game.turn
+    puts "Type 'revenge' to play another round. Type 'exit' to leave the game."
+    revenge = gets.chomp
+    while revenge == "revenge"
+      new_game.new_round
+      new_game.turn
+      puts "Type 'revenge' to play another round. Type 'exit' to leave the game."
+      revenge = gets.chomp  
+    end
   end
-
 end
